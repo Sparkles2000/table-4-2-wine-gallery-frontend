4d53b116de438c7ecad3a673d2742208a6add9f5
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-11 02:48:10 -0500

    Created a form form for Brand Of Wine Form Component.

--- src/Components/Customergroup/CustomergroupForm.js
+++ src/Components/Customergroup/CustomergroupForm.js
@@ -23,14 +23,14 @@ function CustomergroupForm({createCustomergroup}) {
             
             <form onSubmit={ handleSubmit } className="customergroup-form">
   
-  <label className='form-label'>Party</label>
-  <input type="text" placeholder='Party' required value={ party }onChange={(e) => setParty(e.target.value)} />
+    <label className='form-label'>Party</label>
+    <input type="text" placeholder='Type Names In Party Here' required value={ party }onChange={(e) => setParty(e.target.value)} />
 
-<label className='form-label'>Party Quantity</label>
-  <input type="text" placeholder='Party Quantity' required value={ partyquantity } onChange={(e) => setPartyquantity(e.target.value)} />
+    <label className='form-label'>Party Quantity</label>
+    <input type="text" placeholder='Type Party Quantitiy Here' required value={ partyquantity } onChange={(e) => setPartyquantity(e.target.value)} />
 
-  <label className="form-label">Customer Status</label>
-  <input type="text" placeholder='Status' value={ customerstatus }onChange={(e) => setCustomerstatus(e.target.value)} />
+    <label className="form-label">Customer Status</label>
+    <input type="text" placeholder='Type Status Here' value={ customerstatus }onChange={(e) => setCustomerstatus(e.target.value)} />
 
 <button>Submit</button>
 <br />
