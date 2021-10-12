7bfdd0dd61cba057bf43eaf6bfb9d399fe848978
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-12 09:11:30 -0500

    Create a form for Art Piece in Art Piece Form.

---
+++ src/Components/Artpiece/Artpieceform.js
@@ -0,0 +1,42 @@
+// import { useHistory } from "react-router-dom";
+import { useState } from "react";
+
+function ArtpieceForm({createArtpiece}) {
+
+    const [artists, setArtists] = useState("");
+    const [piece, setPiece] = useState("");
+    const [artstyle, setArtstyle] = useState("");
+    const [img_src, setImg_src] = useState("");
+    const Artpiece = {artists, piece, artstyle, img_src};
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createArtpiece(Artpiece)
+    }
+
+    return (
+        <div>
+            
+            <form onSubmit={ handleSubmit } className="Artpiece-form">
+  
+    <label className='form-label'>Artists Names</label>
+    <input type="text" placeholder='Type Artists Name Here' required value={ artists }onChange={(e) => setArtists(e.target.value)} />
+
+    <label className='form-label'>Name Of Piece</label>
+    <input type="text" placeholder='Type Piece Name Here' required value={ piece }onChange={(e) => setPiece(e.target.value)} />
+
+    <label className='form-label'>Piece</label>
+    <input type="text" placeholder='Type The Artstyle Here' required value={ artstyle } onChange={(e) => setArtstyle(e.target.value)} />
+
+    <label className="form-label">Image Title</label>
+    <input type="text" placeholder='Type The Image File Here' value={ img_src }onChange={(e) => setImg_src(e.target.value)} />
+
+<button>Submit</button>
+<br />
+<p>Artists: { artists }, Piece: { piece }, Artstyle: { artstyle }, Image: { img_src }</p>
+</form>          
+</div>
+    );
+}
+
+export default ArtpieceForm;
